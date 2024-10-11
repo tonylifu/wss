94cf72e6a5374a07d0da6998c5594283db8cb0d5
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-10-11 14:38:01 +0100

    initial commit

---
+++ src/main/java/com/webstartrek/wss/AppRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.webstartrek.wss;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class AppRestConfiguration extends Application {
+    
+}
---
+++ src/main/java/com/webstartrek/wss/resources/AppResource.java
@@ -0,0 +1,21 @@
+package com.webstartrek.wss.resources;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee11")
+public class AppResource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
